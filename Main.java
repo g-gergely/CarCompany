12dc427328b8d22888a4057c5521d0e065ddede1
g-gergely <42282407+g-gergely@users.noreply.github.com>
2022-08-16 15:40:24 +0200

    Delete Main.java
    Main.java file is deleted because being a duplicate

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.codecool;
-
-public class Main {
-    public static void main (String[] args){
-        Company myCompany = new Company();
-        myCompany.addingCars();
-        myCompany.ProcessSimulation();
-    }
-}
